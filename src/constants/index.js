9de3e252e70aa6f11c9cfc924e9a7e958475a914
maddy maddox <dhanushh344@gmail.com>
2025-04-07 18:39:35 +0530

    added src folder

---
+++ src/constants/index.js
@@ -0,0 +1,273 @@
+
+import {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  gearXpert,
+  project2,
+  project3,
+  mysql,
+  express,
+  aws,
+  mui,
+  
+  gsap,
+  framer,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  firstTestimonial,
+  secondTestimonial,
+  thirdTestimonial,
+} from '../assets'
+
+
+// Import Tekisky separately
+import tekisky from "../assets/company/tekisky.png";
+
+
+export const navLinks = [
+
+
+  {
+    id: "about",
+    title: "About",
+    
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Pen-Tester",
+    icon: web,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: mobile,
+  },
+  {
+    title: "Video Editor",
+    icon: backend,
+  },
+  {
+    title: "Ui UX Designer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "gsap",
+    icon: gsap,
+  },
+  {
+    name: "framer",
+    icon: framer,
+  },
+
+ 
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Material Ui",
+    icon: mui,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "Express Js",
+    icon: express,
+  },
+  {
+    name: "AWS",
+    icon: aws,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "MySql",
+    icon: mysql,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+ 
+
+];
+
+const experiences = [
+  {
+    title: "CyberSecurity Expert",
+    company_name: "Eye Q Dot Net Pvt ltd.",
+    icon: tekisky,
+    iconBg: "#383E56",
+    date: "March 2024 - present",
+    points: [
+      "Conducted hands-on training sessions and workshops on ethical hacking, penetration testing, and system hardening, guiding learners from fundamentals to advanced techniques",
+      "Successfully mentored 500+ students globally, helping them build real-world cybersecurity skills and launch careers in infosec.",
+      "Provided one-on-one guidance, career advice, and custom learning paths for aspiring security professionals and ethical hackers.",
+      "Designed and delivered industry-relevant curriculums covering web/app vulnerabilities, Linux security, Red Hat systems, and responsible disclosure practices.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Dhanush proved me wrong.",
+    name: "MD Mustaqeem",
+    designation: "Ecommerce",
+    company: "QuickMart",
+    image: firstTestimonial,
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Dhanush does.",
+    name: "Abdul Raheman",
+    designation: "Ecommerce Business",
+    company: "justbuyz",
+    image: secondTestimonial,
+  },
+  {
+    testimonial:
+      "After Dhanush optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "James Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: thirdTestimonial,
+  },
+];
+
+const projects = [
+  {
+    name: "Tekisky Mart",
+    description:
+      "Tekisky Mart is a web-based platform that allows users to search, explore, and purchase a wide range of products from various providers, offering a seamless and efficient shopping experience.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "white-text-gradient",
+      },
+      {
+        name: "node",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+ 
+    ],
+    image: project2,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "GearXpert",
+    description:
+      "GearXpert is your ultimate destination for premium auto parts and accessories, Whether you're an automobile enthusiast or a professional, find everything you need to upgrade, repair.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "emailJs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Gsap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gearXpert,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "GoGroove-Ecommerce",
+    description:
+      "Go-Groove is your ultimate destination for premium products across various categories. Whether you're a savvy shopper or a professional, enhance, and simplify your lifestyle—all in one place!",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express",
+        color: "white-text-gradient",
+      },
+      {
+        name: "node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+    ],
+    image: project3,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
